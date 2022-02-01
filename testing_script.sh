9b0b2cc8db67490c42d89626a8414e4811fbd3d9
Bisweswar Martha <bisweswar.martha@gmail.com>
2022-02-01 19:41:03 +0530

    Fixed testing script

--- testing_script.sh
+++ testing_script.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
 FILENO=$1
 # shift
-mkdir output
+[ -d "output" ] || mkdir output
 touch ./output/out_lexer_$FILENO.txt
-python ./src/scanner.py.py ./tests/test_lexer_$FILENO.c  > ./output/out_lexer_$FILENO.txt
+python ./src/scanner.py ./tests/lexer/test_lexer_$FILENO.c >./output/out_lexer_$FILENO.txt
