6108146364398158681aed692fc5ddcfd9650006
Arka Das <arka.das@qure.ai>
2022-02-16 20:36:57 +0530

    scraping of parser.out done

--- src/parser_error.sh
+++
@@ -1,4 +0,0 @@
- """function_definition : declaration_specifiers declarator declaration_list compound_statement
-    | declaration_specifiers declarator compound_statement
-    | declarator declaration_list compound_statement
-    | declarator compound_statement"""
