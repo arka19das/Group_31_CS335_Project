64997eedafb0deead786b167b0f9b227c3bff59f
Arka Das <arka.das@qure.ai>
2022-02-16 15:30:59 +0530

    test cases addded

---
+++ tests/parser/test_parser_1.c
@@ -0,0 +1,29 @@
+int main()
+{
+    if (c == 0)
+    {
+        c++;
+        if (a == b)
+        {
+            a++;
+        }
+    }
+    else
+    {
+        b++;
+    }
+
+    switch (c)
+    {
+    case 1:
+        a--;
+        break;
+
+    case 2:
+        a + b;
+        break;
+
+    default:
+        c++;
+    }
+}
