84a0f05ec5cf413daa16bfeb7e6b655c90cb0866
karthikedar <59352773+karthikedar@users.noreply.github.com>
2022-02-18 19:56:02 +0530

    Update test_parser_5.c

--- tests/parser/test_parser_5.c
+++ tests/parser/test_parser_5.c
@@ -1,14 +1,34 @@
-// Struct Test
-typedef struct MyStruct {
-    int a;
-    int b;
-    double c;
-} mystruct;
+int main()
+{
+    int  count, n;
+    short *from, *to;
+    short a[39], b[39];
 
-int main(int argc, char **argv) {
-    static struct Mystruct z = {2, 3, 10.0};
-    const int a = z->a;
-    int b = z->b;
-    double c = z->c;
-    return 1;
+    for(n = 0; n < 39; n++) {
+        a[n] = n;
+        b[n] = 0;
+    }
+    from = a;
+    to = b;
+    count = 39;
+    n = (count + 7) / 8;
+    switch (count % 8) {
+    case 0: do { *to++ = *from++;
+    case 7:      *to++ = *from++;
+    case 6:      *to++ = *from++;
+    case 5:      *to++ = *from++;
+    case 4:      *to++ = *from++;
+    case 3:      *to++ = *from++;
+    case 2:      *to++ = *from++;
+    case 1:      *to++ = *from++;
+            } while (--n > 0);
+    }
+    for(n = 0; n < 39; n++)
+    {
+        if(a[n] != b[n])
+        {
+            return 1;
+        }
+    }
+    return 0;
 }
