5bea8c6823ccee4f7a2e0f874f9a73d592356e93
karthikedar <59352773+karthikedar@users.noreply.github.com>
2022-02-18 19:54:18 +0530

    Update test_parser_2.c

--- tests/parser/test_parser_2.c
+++ tests/parser/test_parser_2.c
@@ -1,20 +1,49 @@
+void Array_sort(int *array , int n)
+{ 
+    // declare some local variables
+    int i=0 , j=0 , temp=0;
 
-// Function to print an array
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+    for(i=0 ; i<n ; i++)
+    {
+        for(j=0 ; j<n-1 ; j++)
+        {
+            if(array[j]>array[j+1])
+            {
+                temp        = array[j];
+                array[j]    = array[j+1];
+                array[j+1]  = temp;
+            }
+        }
+    }
 }
- 
-// Driver program to test above functions
+float Find_median(int array[] , int n)
+{
+    float median=0;
+    
+    // if number of elements are even
+    if(n%2 == 0)
+    {
+        median = (array[(n-1)/2] + array[n/2])/2.0;
+    }
+    // if number of elements are odd
+    else
+    {
+        median = array[n/2];
+    }
+    
+    return median;
+}
 int main()
 {
-    int arr[] = {10, 7, 8, 9, 1, 5};
+    int arr[] = {11,9,8,5,3,1,1,6,10,5,3,1,2};
+    int i=0, sum=0;
     int n = sizeof(arr)/sizeof(arr[0]);
-    quickSort(arr, 0, n-1);
-    printf("Sorted array: \n");
-    printArray(arr, n);
+    while (i<n)
+    {
+        i++;
+        sum += i;
+    }
+    Array_sort(arr,n);
+    float f =Find_median(arr,n);
     return 0;
 }
