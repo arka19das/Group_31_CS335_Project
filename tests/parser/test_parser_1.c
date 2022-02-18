1863991f70ca644d287064a8678c44c6f923dc09
karthikedar <59352773+karthikedar@users.noreply.github.com>
2022-02-18 19:53:44 +0530

    Update test_parser_1.c

--- tests/parser/test_parser_1.c
+++ tests/parser/test_parser_1.c
@@ -1,29 +1,70 @@
-int main()
+int
+foo(int x[100])
 {
-    if (c == 0)
-    {
-        c++;
-        if (a == b)
-        {
-            a++;
-        }
-    }
-    else
-    {
-        b++;
-    }
+	int y[100];
+	int *p;
+	
+	y[0] = 2000;
+	
+	if(x[0] != 1000)
+	{
+		return 1;
+	}
+	
+	p = x;
+	
+	if(p[0] != 1000)
+	{
+		return 2;
+	}
+	
+	p = y;
+	
+	if(p[0] != 2000)
+	{
+		return 3;
+	}
+	
+	if(sizeof(x) != sizeof(void*))
+	{
+		return 4;
+	}
+	
+	if(sizeof(y) <= sizeof(x))
+	{
+		return 5;
+	}
+	
+	return 0;
+}
 
-    switch (c)
-    {
-    case 1:
-        a--;
-        break;
 
-    case 2:
-        a + b;
-        break;
 
-    default:
-        c++;
-    }
+int
+main()
+{
+    long i;
+	unsigned long u;
+
+	i = 1;
+	i = -1;
+	i = -1L;
+	i = -1u;
+	i = -1l;
+	i = (1ll << 32) - 1 & 3;
+	i = (long) ((1ull << 32) - 1) < 0;
+	i = -1u < 0;
+
+	u = 1;
+	u = -1;
+	u = -1l;
+	u = -1ull;
+	u = -1ll;
+	u = (1ll << 32) - 1 & 3;
+	u = (long) ((1ll << 32) - 1) < 0;
+	u = -1u < 0;
+	int x[100];
+	x[0] = 1000;
+	
+	return foo(x);
 }
