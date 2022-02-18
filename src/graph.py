b59c71e4536af887ea872fad202b2508dc218822
Bisweswar Martha <bisweswar.martha@gmail.com>
2022-02-18 21:15:28 +0530

    Added Parse subcase to testing_script.sh

--- src/graph.py
+++ src/graph.py
@@ -2,39 +2,40 @@ import regex as re
 import pydot
 import sys
 
-original_stdout = sys.stdout
-file = open("parser.out", "r")
-text = file.read()
-split_text = re.split(r"\nstate (\d+)\n", text)
+def main(input_path, output_path):
+    original_stdout = sys.stdout
+    file = open(input_path, "r")
+    text = file.read()
+    split_text = re.split(r"\nstate (\d+)\n", text)
 # there are 0-360  361 total states
 
-shift_dict = {}
-reduce_dict = {}
+    shift_dict = {}
+    reduce_dict = {}
 # print(split_text[0])
 
-for i in range(2, 723, 2):
-    matching_text = re.findall(
-        "(\w+)(\s+) shift and go to state (\d+)\n", split_text[i]
-    )
-    for items in matching_text:
-        shift_dict[int(split_text[i - 1]), int(items[2])] = items[0]
-    matching_text1 = re.findall("(\w+)(\s+) reduce using rule (\d+)", split_text[i])
+    for i in range(2, 723, 2):
+        matching_text = re.findall(
+            "(\w+)(\s+) shift and go to state (\d+)\n", split_text[i]
+        )
+        for items in matching_text:
+            shift_dict[int(split_text[i - 1]), int(items[2])] = items[0]
+        matching_text1 = re.findall("(\w+)(\s+) reduce using rule (\d+)", split_text[i])
 
-    # for items in matching_text1:
-    #     reduce_dict[int(split_text[i - 1]), int(items[2])] = items[0]
+        # for items in matching_text1:
+        #     reduce_dict[int(split_text[i - 1]), int(items[2])] = items[0]
 
 
-graph = pydot.Dot("Grammar_graph", graph_type="digraph", bgcolor="white")
+    graph = pydot.Dot("Grammar_graph", graph_type="digraph", bgcolor="white")
 
-for i in range(0, 361):
-    my_node = pydot.Node(str(i), label=f"I{i}", shape="circle",style="filled",fillcolor="yellow")
-    graph.add_node(my_node)
+    for i in range(0, 361):
+        my_node = pydot.Node(str(i), label=f"I{i}", shape="circle",style="filled",fillcolor="yellow")
+        graph.add_node(my_node)
 
-for key, value in shift_dict.items():
-    # print(key)
-    graph.add_edge(pydot.Edge(str(key[0]), str(key[1]), label=value, color="black"))
+    for key, value in shift_dict.items():
+        # print(key)
+        graph.add_edge(pydot.Edge(str(key[0]), str(key[1]), label=value, color="black"))
 
-graph.write_raw("output_raw.dot")
+    graph.write_raw(output_path)
 
 #graph.write_png("output.png")
 
@@ -48,4 +49,7 @@ graph.write_raw("output_raw.dot")
 #     print(reduce_dict)
 #     sys.stdout = original_stdout
 
-file.close()
+    file.close()
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2])
--- testing_script.sh
+++ testing_script.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-FILENO=$1
-# shift
+lex() {
+    [ -d "output/lexer" ] || mkdir -p output/lexer
+    python ./src/scanner.py ./tests/lexer/test_lexer_"$1".c > ./output/lexer/out_lexer_"$1".txt 
+}
+
+parse() {
+    [ -d "output/parser" ] || mkdir -p output/parser
+    [ -f "src/parsetab.py" ] && rm src/parsetab.py
+    python ./src/parser.py -input ./tests/parser/test_parser_"$1".c
+    mv ./src/parser.out ./output/parser/out_parser_"$1".out
+    python ./src/graph.py ./output/parser/out_parser_"$1".out \
+        ./output/parser/out_parser_"$1".dot
+    sfdp -x -Goverlap=scale -Tsvg ./output/parser/out_parser_"$1".dot\
+        -o ./output/parser/out_graph_"$1".svg
+}
+
+usage="./testing_script.sh <lex|parse> <test_number>"
+
 [ -d "output" ] || mkdir output
-touch ./output/out_lexer_$FILENO.txt
-python ./src/scanner.py ./tests/lexer/test_lexer_$FILENO.c >./output/out_lexer_$FILENO.txt
+
+if [[ $# -ne 2 ]]; 
+    then echo "$usage" && exit
+fi
+
+case $1 in 
+    "lex") lex "$2" ;;
+    "parse") parse "$2" ;;
+    *) echo "$usage";;
+esac
