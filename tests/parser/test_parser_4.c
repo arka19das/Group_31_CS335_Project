4f72903d46f5971ecbf0c0d3f721194082dc943c
karthikedar <59352773+karthikedar@users.noreply.github.com>
2022-02-18 19:55:21 +0530

    Update test_parser_4.c

--- tests/parser/test_parser_4.c
+++ tests/parser/test_parser_4.c
@@ -1,20 +1,19 @@
-// testing loops
-
-int main()
-{
-    int i=0;
-    int n = 10;
-    int sum = 0;
-    for (i = 0; i< n; i++)
-    {
-        sum += i;
-    }
-
-    while (i>0)
-    {
-        i--;
-        sum -= i;
-    }
-
-    return 0;
-}
+long factorial(int n)  
+{  
+  if (n == 0)  
+   { return 1;
+   }  
+  else  
+  {
+    return(n * factorial(n-1));
+  }  
+}  
+   
+void main()  
+{  
+  int number=1024;  
+  long fact;  
+   
+  fact = factorial(number);    
+  return 0;  
+}  
