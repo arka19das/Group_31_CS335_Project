44461b7db694debf866d4a864dbeb63fb3ca0f53
A Kedarnath <59352773+karthikedar@users.noreply.github.com>
2022-04-16 14:54:23 +0530

    Handled binary, conditional operators in TYPE_INT

--- src/mips.py
+++ src/mips.py
@@ -17,8 +17,12 @@ def is_int(x):
 #print(is_int("$(fp)"))        
 
 TYPE_INT = [
-    "char","unsigned char", "short","unsigned short", "int", "unsigned int", "long", "unsigned long"
+    "char", "unsigned char", "short", "unsigned short", "int", "unsigned int", "long", "unsigned long"
 ]
+
+#TYPE_LONG = []
+
+BIN_BIT_OP = ["&","|","<<",">>","^"]
 
 STORE_INSTRUCTIONS = {
     "char": "SW",
@@ -59,16 +63,16 @@ Binary_ops = {
    
     #"unsigned int<=": ["SLTU","XORI $2 $2 0x1" 4],
     #SLE not there in edumips  
-    "unsigned int<=": ["SLEU", 4],
+    "unsigned int<=": ["SLTU", 4],
     "unsigned int<": ["SLTU", 4],
     #SGE and SGT not there in edumips, have to use SLTU changing the seq of registers
-    "unsigned int>=": ["SGEU", 4],
-    "unsigned int>": ["SGTU", 4],
+    "unsigned int>=": ["SLTU", 4],
+    "unsigned int>": ["SLTU", 4],
     #SEQU, SNEU not there in edumips
     #"unsigned int==": ["XOR","SLTU $2 $2 1" 4],
-    "unsigned int==": ["SEQU", 4],
+    "unsigned int==": ["XOR", 4],
     #"unsigned int!=": ["XOR","SLTU $2 $0 $2" 4],
-    "unsigned int!=": ["SNEU", 4],
+    "unsigned int!=": ["XOR", 4],
     
     "unsigned int&": ["AND", 4],
     "unsigned int^": ["XOR", 4],
@@ -89,12 +93,12 @@ Binary_ops = {
     "int/": ["DIV", 4],
     "int%": ["DIV",4],
 
-    "int<=": ["SLE", 4],
+    "int<=": ["SLT", 4],
     "int<": ["SLT", 4],
-    "int>=": ["SGE", 4],
-    "int>": ["SGT", 4],
-    "int==": ["SEQ", 4],
-    "int!=": ["SNE", 4],
+    "int>=": ["SLT", 4],
+    "int>": ["SLT", 4],
+    "int==": ["XOR", 4],
+    "int!=": ["XOR", 4],
 
     "int&": ["AND", 4],
     "int^": ["XOR", 4],
@@ -117,12 +121,12 @@ Binary_ops = {
     "short/": ["DIV", 2],
     "short%": ["DIV", 2],
 
-    "short<=": ["SLE", 2],
+    "short<=": ["SLT", 2],
     "short<": ["SLT", 2],
-    "short>=": ["SGE", 2],
-    "short>": ["SGT", 2],
-    "short==": ["SEQ", 2],
-    "short!=": ["SNE", 4],
+    "short>=": ["SLT", 2],
+    "short>": ["SLT", 2],
+    "short==": ["XOR", 2],
+    "short!=": ["XOR", 4],
    
     "short&": ["AND", 2],
     "short^": ["XOR", 2],
@@ -144,16 +148,16 @@ Binary_ops = {
     
     #"unsigned int<=": ["SLTU","XORI $2 $2 0x1" 4],
     #SLE not there in edumips
-    "unsigned short<=": ["SLEU", 2],
+    "unsigned short<=": ["SLTU", 2],
     "unsigned short<": ["SLTU", 2],
     #SGE and SGT not there in edumips, have to use SLTU changing the seq of registers
-    "unsigned short>=": ["SGEU", 2],
-    "unsigned short>": ["SGTU", 2],
+    "unsigned short>=": ["SLTU", 2],
+    "unsigned short>": ["SLTU", 2],
     #SEQU, SNEU not there in edumips
     #"unsigned int==": ["XOR","SLTU $2 $2 1" 4],
-    "unsigned short==": ["SEQU", 2],
+    "unsigned short==": ["XOR", 2],
     #"unsigned int!=": ["XOR","SLTU $2 $0 $2" 4],
-    "unsigned short!=": ["SNEU", 2],
+    "unsigned short!=": ["XOR", 2],
     
     "unsigned short&": ["AND", 2],
     "unsigned short^": ["XOR", 2],
@@ -175,16 +179,16 @@ Binary_ops = {
     
     #"unsigned int<=": ["SLTU","XORI $2 $2 0x1" 4],
     #SLE not there in edumips
-    "unsigned char<=": ["SLEU", 4],
+    "unsigned char<=": ["SLTU", 4],
     "unsigned char<": ["SLTU", 4],
     #SGE and SGT not there in edumips, have to use SLTU changing the seq of registers
-    "unsigned char>=": ["SGEU", 4],
-    "unsigned char>": ["SGTU", 4],
+    "unsigned char>=": ["SLTU", 4],
+    "unsigned char>": ["SLTU", 4],
     #SEQU, SNEU not there in edumips
     #"unsigned int==": ["XOR","SLTU $2 $2 1" 4],
-    "unsigned char==": ["SEQU", 4],
+    "unsigned char==": ["XOR", 4],
     #"unsigned int!=": ["XOR","SLTU $2 $0 $2" 4],
-    "unsigned char!=": ["SNEU", 4],
+    "unsigned char!=": ["XOR", 4],
     
     "unsigned char&": ["AND", 4],
     "unsigned char^": ["XOR", 4],
@@ -205,12 +209,12 @@ Binary_ops = {
     "char/": ["DIV", 4],
     "char%": ["DIV", 4],
     
-    "char<=": ["SLE", 4],
+    "char<=": ["SLT", 4],
     "char<": ["SLT", 4],
-    "char>=": ["SGE", 4],
-    "char>": ["SGT", 4],
-    "char==": ["SEQ", 4],
-    "char!=": ["SNE", 4],
+    "char>=": ["SLT", 4],
+    "char>": ["SLT", 4],
+    "char==": ["XOR", 4],
+    "char!=": ["XOR", 4],
     
     "char&": ["AND", 4],
     "char^": ["XOR", 4],
@@ -224,41 +228,61 @@ Binary_ops = {
     # "char||": ["NOR", 4],
     
 
-    #LONG, DOUBLE, FLOAT, UNSIGNED LONG NOT HANDLED WITH NEW OPERATORS
     #Changes similar to unsigned int
     "unsigned long+": ["DADDU", 8],
     "unsigned long-": ["DSUBU", 8],
+    
     "unsigned long*": ["DMULTU", 8],
     "unsigned long/": ["DDIVU", 8],
-    "unsigned long<=": ["SLEU", 8],
+    "unsigned long%": ["DDIVU", 8],
+
+    "unsigned long<=": ["SLTU", 8],
     "unsigned long<": ["SLTU", 8],
-    "unsigned long>=": ["SGEU", 8],
-    "unsigned long>": ["SGTU", 8],
-    "unsigned long==": ["SEQU", 8],
-    "unsigned long!=": ["SNEU", 8],
+    "unsigned long>=": ["SLTU", 8],
+    "unsigned long>": ["SLTU", 8],
+    "unsigned long==": ["XOR", 8],
+    "unsigned long!=": ["XOR", 8],
+    
     "unsigned long&": ["AND", 8],
     "unsigned long^": ["XOR", 8],
     "unsigned long|": ["OR", 8],
-    "unsigned long!": ["NOR", 8],
     "unsigned long~": ["NOT", 8],
+    "unsigned long>>": ["DSRL", 8],
+    "unsigned long<<": ["DSLL", 8],
+
+    # "unsigned long!": ["NOR", 8],
+    # "unsigned long&&": ["NOR", 8],
+    # "unsigned long||": ["NOR", 8],
 
     #Changes similar to unsigned int
     "long+": ["DADD", 8],
     "long-": ["DSUB", 8],
+    
     "long*": ["DMULT", 8],
     "long/": ["DDIV", 8],
-    "long<=": ["SLE", 8],
+    "long%": ["DDIV",8],
+    
+    "long<=": ["SLT", 8],
     "long<": ["SLT", 8],
-    "long>=": ["SGE", 8],
-    "long>": ["SGT", 8],
-    "long==": ["SEQ", 8],
-    "long!=": ["SNE", 8],
+    "long>=": ["SLT", 8],
+    "long>": ["SLT", 8],
+    "long==": ["XOR", 8],
+    "long!=": ["XOR", 8],
+    
     "long&": ["AND", 8],
     "long^": ["XOR", 8],
     "long|": ["OR", 8],
-    "long!": ["NOR", 8],
+    "long<<": ["DSLL", 8],
+    "long>>": ["DSRL", 8],
     "long~": ["NOT", 8],
 
+    # "long!": ["NOR", 8],
+    # "long||": ["NOR", 8],
+    # "long&&": ["NOR", 8],
+
+
+
+    # DOUBLE, FLOAT, NOT HANDLED WITH NEW OPERATORS
     #<=, >, >=,!= pata nahi kya karenge
     "double+": ["ADD.D", 8],
     "double-": ["SUB.D", 8],
@@ -267,7 +291,7 @@ Binary_ops = {
     "double<=": ["SLE", 8],
     "double<": ["C.LT.D", 8],
     "double>=": ["SGE", 8],
-    "double>": ["SGT", 8],
+    "double>": ["SLT", 8],
     "double==": ["C.EQ.D", 8],
     "double!=": ["SNE", 8],
 
@@ -279,7 +303,7 @@ Binary_ops = {
     "float<=": ["SLE", 4],
     "float<": ["C.LT.D", 4],
     "float>=": ["SGE", 4],
-    "float>": ["SGT", 4],
+    "float>": ["SLT", 4],
     "float==": ["C.EQ.D", 4],
     "float!=": ["SNE", 4],
     
@@ -287,7 +311,7 @@ Binary_ops = {
     "+": ["ADD", 4],
 }
 
-
+#Handled type_int with [+,-,%,&,|,>>,<<,^,~,>,>=,<=,<,==,!=]
 def binary_exp_mips(binexp, reg1, a1, reg2, a2, reg3, a3):
     mips = []
     type = ""
@@ -297,19 +321,51 @@ def binary_exp_mips(binexp, reg1, a1, reg2, a2, reg3, a3):
             type += i
         else:
             op+= i
+    mips.append(load_reg(reg1, a1, type))
+    mips.append(load_reg(reg2, a2, type))
+    mips.append(load_reg(reg3, a3, type))        
     if (op =="+" or op == "-") and type in TYPE_INT :        
-        #mips.append(["LW",      {reg1},    {a1}])
-        mips.append(load_reg(reg1, a1, type))
-        #mips.append(["LW",      {reg2},    {a2}])
-        mips.append(load_reg(reg2, a2, type))
-        #mips.append(["LW",      {reg3},    {a3}])
-        mips.append(load_reg(reg3, a3, type))
-        #mips.append(["ADD",     {reg1},    {reg2},    {reg3}])
-        op = Binary_ops[binexp]
+        op = Binary_ops[binexp][0]
         mips.append([op,reg1,reg2,reg3])
-        #mips.append(["SW",      {reg1},    {a1}])
-        mips.append(store_reg(reg1, a1, type))
-        return mips
+    elif op =="%" and type in TYPE_INT :
+        op = Binary_ops[binexp][0]
+        mips.append([op,reg2,reg3])
+        mips.append(["MFHI",reg1])
+    elif op in BIN_BIT_OP and type in TYPE_INT:
+        op = Binary_ops[binexp][0]
+        mips.append([op,reg1,reg2,reg3])
+    elif op == "~" and type in TYPE_INT:
+        #only 2 regs will be given
+        op = Binary_ops[binexp][0]
+        mips.append([op,reg1,"$0",reg2])
+    elif op == "<" and type in TYPE_INT:
+        op = Binary_ops[binexp][0]
+        mips.append([op, reg1, reg2, reg3])
+    elif op == ">" and type in TYPE_INT:
+        op = Binary_ops[binexp][0]
+        mips.append([op, reg1, reg3, reg2])
+    elif op == "<=" and type in TYPE_INT:
+        op = Binary_ops[binexp][0]
+        #check greater than and xor with 1
+        mips.append([op, reg1, reg3, reg2])
+        mips.append(["XORI",reg1,reg1,"0x1"])
+    elif op == ">=" and type in TYPE_INT:
+        op = Binary_ops[binexp][0]
+        #check less than and xor with 1
+        mips.append([op, reg1, reg2, reg3])
+        mips.append(["XORI", reg1, reg1, "0x1"])
+    elif op == "==" and type in TYPE_INT:
+        op = Binary_ops[binexp][0]
+        mips.append([op, reg1, reg2, reg3])
+        mips.append(["SLTUI",reg1,reg1,"1"])
+    elif op == "!=" and type in TYPE_INT:
+        op = Binary_ops[binexp][0]
+        mips.append([op, reg1, reg2, reg3])
+        mips.append(["SLTUI",reg1,"$0",reg1])    
+
+    mips.append(store_reg(reg1, a1, type))
+    return mips
+         
 
 
 def store_reg(reg,addr,type):
@@ -322,6 +378,13 @@ def load_reg(reg,addr,type):
     #mips = []
     instr = STORE_INSTRUCTIONS[type]
     if(is_int(addr)):
-        instr = "LI"
+        if type == "int" or type == "char" or type =="short":
+            mips = ["ADDI",reg,"$0",addr]
+        elif type == "long":
+            mips = ["DADDI",reg,"$0",addr]
+        elif type == "unsigned int" or type == "unsigned char" or type =="unsigned short":
+            mips = ["ADDIU",reg,"$0",addr]
+        elif type == "unsigned long":
+            mips = ["DADDIU",reg,"$0",addr]      
     mips = [instr, reg, addr]
     return mips
