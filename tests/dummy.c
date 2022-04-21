9211b3c66fdee5160ba3a556020135020b2c5195
Bisweswar Martha <bisweswar.martha@gmail.com>
2022-04-22 00:13:00 +0530

    Revert "Function offsets corercted"
    This reverts commit 4bc52ab8c7bf55b55eb706196cd13e98d3a4cb30.

--- tests/dummy.c
+++ tests/dummy.c
@@ -1,28 +1,10 @@
-#include <stdio.h>
-struct t1
-{
-    int a;
-    struct t1 *next;
-    int b;
-    double d1;
-    // struct t1 *next;
-};
 
-int func(int p, long int j, struct t1 d, int k) //(int a, struct t1 b, int c, int d, int e, int f)
-{
-    int m, n;
-    // d++;
-    m = p;
-    // j++;
-    // k++;
-    // m++;
-    n >>= 1;
+// #include<stdio.h>
+// int j=0;
+int foo(int a){
+    return 0;
 }
-int main()
-{
-    struct t1 d1;
-    int  a, x, b;
-    func(a, x, d1, b);
-    
+int main(){
+    foo(1);
     return 0;
 }
